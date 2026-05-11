c43e58c012b4450a61bf388fdcc190547ee5bf6b
RAJILA BABU <RAJILA BABU@JERRY.lan>
2026-05-11 12:50:17 +0530

    second commit

--- src/add1/j4.java
+++ src/add1/j4.java
@@ -6,5 +6,6 @@ public static void main (String[] arg)
   System.out.println("ADDRESS:Nagercoil,Parvathipuram,629222");
   System.out.println("PHONE: 9344888474" );
   System.out.println("Alo info technology");
+  System.out.println("fdygedhjpojode");
   }
 }
